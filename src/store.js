4d035497db5f2490884335106707bfc080a43834
tmcinerney4 <tmack.iv@gmail.com>
2017-03-14 17:10:13 -0500

    middleware for redux state added

--- src/store.js
+++ src/store.js
@@ -1,16 +1,20 @@
-import {createStore, applyMiddleware} from 'redux';
+  import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(
     thunkMiddleware,
     createLogger({collapsed:true})
   )
-);
+))
+
+
 
 export default store;
