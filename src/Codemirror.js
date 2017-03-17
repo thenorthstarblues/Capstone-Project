2f2fe9e1f730f82ab13c4b0b03d5c642b26aeb31
ray coti <Raymond.coti@gmail.com>
2017-03-17 15:11:03 -0500

    most updated html conversion

--- src/Codemirror.js
+++ src/Codemirror.js
@@ -4,35 +4,6 @@ require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/xml/xml');
 require('codemirror/mode/markdown/markdown');
 require('codemirror/lib/codemirror.css')
-//import {connect} from 'react-redux'
-
-//var htmlData = require('./fsRead').data || `//display code here`
-
-
-
-
-//this is codemirror supplied code for testing purposes
-/*const codetest = React.createClass({
-    getInitialState: function() {
-        return {
-            code: htmlData,
-        };
-    },
-    updateCode: function(newCode) {
-        this.setState({
-            code: newCode,
-        });
-    },
-    render: function() {
-        const options = {
-            lineNumbers: true,
-        };
-        return <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
-    }
-});
-export {codetest}
-*/
-
 
   //this is real code
 const Code = (props) => {
