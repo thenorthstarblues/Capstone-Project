108f8fb8700f96aea8bae5608b9343ac01e69311
siteations <studer@siteations.com>
2017-03-17 13:55:54 -0500

    dummyData done (really) and starting the sibling recursion

--- src/reducers/siblingReducer.js
+++ src/reducers/siblingReducer.js
@@ -13,8 +13,6 @@ export const findSiblings = (objects => { // one giant object, with each id-obje
 
 	//the magic happens here!
 
-
-
   return {
     type: SIB_RECOG,
     objects:boxes,
@@ -22,36 +20,18 @@ export const findSiblings = (objects => { // one giant object, with each id-obje
 
 });
 
-export const findParents = (objects => { // one giant object, with each id-object as held
-
-  const boxes=Object.assign( {}, objects); //clone and begin manipulation
-
-  //the magic happens here!
-
-
-
-  return {
-    type: PARENT_RECOG,
-    objects:boxes,
-  }
-
-});
 
 //-----------------------initial state ...stuffing state------------
 
-//see above. . .
+const initialState = dummyData0;
 
 //---------------------------action reducer---------------------------
 
-const siblingReducer = (prevState = {}, action) => {
+const siblingReducer = (prevState = initialState, action) => {
   let nextState = Object.assign({}, prevState);
 
   switch(action.type) {
     case SIB_RECOG:
-      nextState.boxes = action.boxes;
-      break;
-
-    case PARENT_RECOG:
       nextState.boxes = action.boxes;
       break;
 
