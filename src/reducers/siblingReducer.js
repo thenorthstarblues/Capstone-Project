90bb245c78912209ab045a0ffbab55e831a5976d
siteations <studer@siteations.com>
2017-03-17 16:29:28 -0500

    orderly sibling in the works

--- src/reducers/siblingReducer.js
+++ src/reducers/siblingReducer.js
@@ -3,13 +3,14 @@ import {dummyData0, dummyData1} from './testingOnly/dummyLayoutData.js';
 
 
 export const SIB_RECOG = 'SIB_RECOG';
-export const PARENT_RECOG = 'PARENT_RECOG';
 
 //---------------------------action creator---------------------------
 
 export const findSiblings = (objects => { // one giant object, with each id-object as held
 
-  const boxes=Object.assign( {}, objects); //clone and begin manipulation
+  const boxObjs=Object.assign({}, objects); //clone and begin manipulation
+
+  //add function below and include here...
 
 	//the magic happens here!
 
@@ -23,7 +24,7 @@ export const findSiblings = (objects => { // one giant object, with each id-obje
 
 //-----------------------initial state ...stuffing state------------
 
-const initialState = dummyData0;
+const initialState = dummyData0; // just for initial testing
 
 //---------------------------action reducer---------------------------
 
@@ -42,4 +43,68 @@ const siblingReducer = (prevState = initialState, action) => {
 }
 
 export default siblingReducer;
+
+//for each obj, run this on its children
+
+//a:{children:[]}
+
+function sibCheck(object, children){
+
+  if (children.length<2){ // 1 child, like an image or something... but nothing to be reordered
+    //do nothing
+    break;
+  }
+
+  if (children.length>=2){
+    // [b,c,d,e]
+    /* for each, pop from array and see whether: b dimension , then check some [c,d,e]
+      check some - remaining objects align in column ... make those objects their own array */
+    function withinC(child){
+      return (child.x === comp.x && (child.x + child.width)===(comp.x + comp.width));
+    }
+
+    function withoutC(child){
+      return !withinC(child);
+    }
+
+    function withinR(child){
+      return (child.y === comp.y && (child.y + child.height)===(comp.y + comp.height));
+    }
+
+    function withoutR(child){
+      return !withinR(child);
+    }
+
+    var comp = children.shift();
+
+    var col = children.filter(withinC); // any fit as columns ? if so return in array
+    children = children.filter(withoutC); //copy those that don't fit in column
+
+    if (col){
+      col.push(comp);
+      //reset parent and child attributes w/ new div.
+      //col are the column-children
+
+    };
+
+    comp = children.shift();
+
+    var row = children.filter(withinR); // any fit as columns ? if so return in array
+    children = children.filter(withoutR); //copy those that don't fit in either set
+
+    if (row){
+      row.push(comp);
+      //reset parent and child attributes w/ new div.
+      //col are the column-children
+
+    };
+    /*for those objects that that don't match
+    new check some -
+
+    */
+
+  }
+
+
+}
 
