b86ece3d81082fa8f01731ae8380ac1a0676a4e5
Thomas <tmack.iv@gmail.com>
2017-03-20 15:25:02 -0500

    3 working tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TrashCan from './components/TrashCan';
+import AddOptions from './components/AddOptions';
+import Box from './components/Box'
 
-it('renders without crashing', () => {
+
+it('Renders TrashCan component', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<TrashCan />, div);
 });
+
+
+it('loads all Options', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AddOptions />, div);
+});
+
+
+it('renders Box', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Box />, div);
+});
+
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
+
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
