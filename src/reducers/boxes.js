2a3efa6368d0d3fd4f727310c3ee3c6c57cce37a
David Martin <davidjmartin01@gmail.com>
2017-03-23 11:42:41 -0500

    first changes using immutable js on boxes reducer

--- src/reducers/boxes.js
+++ src/reducers/boxes.js
@@ -1,104 +1,94 @@
-import axios from 'axios'
-const initialState = {
-  0: {
+import axios from 'axios';
+import { Map, fromJS } from 'immutable';
+
+const initialState = Map({
+  0: Map({
     height: 500,
     width: 950,
-    children: [],
+    children: Map(),
     id: 0,
     x: 0,
     y: 0,
     parent: null,
     tag: 'div',
     css: '',
-  }
-};
+  }),
+});
 
-const SAVE = 'SAVE'
-const ADD_BOX = 'ADD_BOX'
-const REMOVE_BOX = 'REMOVE_BOX'
-const SET_BOX = 'SET_BOX'
-const SET_PARENT = 'SET_PARENT'
-const ADD_CHILD = 'ADD_CHILD'
-const REMOVE_PARENT = 'REMOVE_PARENT'
-const REMOVE_CHILD = 'REMOVE_CHILD'
-const COPY_BOX = 'COPY_BOX'
-const LOAD_LAYOUT = 'LOAD_LAYOUT'
+const SAVE = 'SAVE';
+const ADD_BOX = 'ADD_BOX';
+const REMOVE_BOX = 'REMOVE_BOX';
+const SET_BOX = 'SET_BOX';
+const SET_PARENT = 'SET_PARENT';
+const ADD_CHILD = 'ADD_CHILD';
+const REMOVE_PARENT = 'REMOVE_PARENT';
+const REMOVE_CHILD = 'REMOVE_CHILD';
+const COPY_BOX = 'COPY_BOX';
+const LOAD_LAYOUT = 'LOAD_LAYOUT';
 
 
-export const setBox = (box) => {
-  return {
-    type: SET_BOX,
-    box
-  }
-}
+export const setBox = box => ({
+  type: SET_BOX,
+  box,
+});
 
-export const addBox = (id, tag) => {
-  return {
-    type: ADD_BOX,
-    box: {
-      id: +id,
-      x: 960,
-      y: 100,
-      width: 100,
-      height: 50,
-      children: [],
-      parent: null,
-      tag: tag,
-      css: 'p10 ',
-    }
-  }
-}
 
-export const removeBox = (boxId) => {
-  return {
-    type: REMOVE_BOX,
-    boxId,
-  }
-}
+export const addBox = (id, tag) => ({
+  type: ADD_BOX,
+  box: {
+    id: +id,
+    x: 960,
+    y: 100,
+    width: 100,
+    height: 50,
+    children: Map(),
+    parent: null,
+    tag,
+    css: 'p10 ',
+  },
+});
 
-export const setParent = (parentId, childId) => {
-  return {
-    type: SET_PARENT,
-    parentId,
-    childId
-  }
-}
 
-export const addChild = (parentId, childId) => {
-  return {
-    type: ADD_CHILD,
-    parentId,
-    childId
-  }
-}
+export const removeBox = boxId => ({
+  type: REMOVE_BOX,
+  boxId,
+});
 
-export const removeParent = (childId) => {
-  return {
-    type: REMOVE_PARENT,
-    childId
-  }
-}
 
-export const removeChild = (parentId, childId) => {
-  return {
-    type: REMOVE_CHILD,
-    parentId,
-    childId
-  }
-}
+export const setParent = (parentId, childId) => ({
+  type: SET_PARENT,
+  parentId,
+  childId,
+});
 
-export const copyBox = (boxId, newBoxId) => {
-  return {
-    type: COPY_BOX,
-    boxId,
-    newBoxId,
-  }
-}
+
+export const addChild = (parentId, childId) => ({
+  type: ADD_CHILD,
+  parentId,
+  childId,
+});
+
+export const removeParent = childId => ({
+  type: REMOVE_PARENT,
+  childId,
+});
+
+export const removeChild = (parentId, childId) => ({
+  type: REMOVE_CHILD,
+  parentId,
+  childId,
+});
+
+export const copyBox = (boxId, newBoxId) => ({
+  type: COPY_BOX,
+  boxId,
+  newBoxId,
+});
 
 export const load = (newLayout) =>{
   return {
     type: LOAD_LAYOUT,
-    newLayout, //TODO: make this dispatch to load 
+    newLayout, //TODO: make this dispatch to load
   }
 }
 
@@ -109,43 +99,39 @@ export const save = ()=>{
   }
 }
 
-//reducer
 const boxesReducer = (prevState = initialState, action) => {
-  const newState = Object.assign({}, prevState);
-
-  switch (action.type){
+  switch (action.type) {
     case SET_BOX:
-      newState[action.box.id] = action.box;
-      break;
+      return prevState.set(action.box.id, fromJS(action.box));
     case ADD_BOX:
-      newState[action.box.id] = action.box;
-      break;
+      return prevState.set(action.box.id, fromJS(action.box));
     case REMOVE_BOX:
-      delete newState[action.boxId];
-      break;
+      return prevState.delete(action.box.id);
     case SET_PARENT:
-      newState[+action.childId].parent = action.parentId;
-      break;
+      return prevState.setIn([action.childId, 'parent'], action.parentId);
     case ADD_CHILD:
-        if(!newState[action.parentId].children.includes(action.childId)) newState[action.parentId].children.push(action.childId);
+      if (!prevState[action.parentId].children.includes(action.childId)) {
+        return prevState.parentId.update('children', (childMap) => {
+          return childMap.push(action.childId);
+        });
+      }
       break;
     case REMOVE_PARENT:
-      newState[action.childId].parent = null;
-      break;
+      return prevState.setIn([action.childId, 'parent'], null);
     case REMOVE_CHILD:
-      newState[action.parentId].children = newState[action.parentId].children.filter(id => id != action.childId);
-      break;
+      return prevState.parentId.update('children', (childMap) => {
+        return childMap.filter(child => child.id !== action.childId);
+      });
     case COPY_BOX:
-      const copyOfBox = Object.assign({}, newState[action.boxId])
-      const newBox = Object.assign(copyOfBox, {
-        id: action.newBoxId,
-        x: 960,
-        y: 100,
-        children: [],
-        parent: null,
-      })
-      newState[action.newBoxId] = newBox;
-      break;
+      const newBox = prevState[action.boxId].withMutations((oldBox) => {
+        oldBox
+          .set('id', action.newBoxId)
+          .set('x', 960)
+          .set('y', 100)
+          .set('children', Map())
+          .set('parent', null);
+      });
+      return prevState.set(action.newBoxId, newBox);
     case LOAD_LAYOUT:
       return action.newLayout; // this might work
     case SAVE:
@@ -153,8 +139,7 @@ const boxesReducer = (prevState = initialState, action) => {
     default:
       return prevState;
   }
-  return newState;
-}
+};
 
 export default boxesReducer;
 
@@ -191,7 +176,7 @@ export const saveLayout = (name, stateCopy) => {
     })
     .then((layout)=> {
       const id = layout.data.id
-      const makeelements =[]; //converting to array 
+      const makeelements =[]; //converting to array
       const elemClone = Object.assign({},stateCopy);
       const elementIdArr = Object.keys(elemClone);
       for (var i = 0; i <elementIdArr.length; i++){
@@ -204,7 +189,7 @@ export const saveLayout = (name, stateCopy) => {
       }
 
       Promise.all(makeelements).then((result)=>{
-        
+
       dispatch(loadLayout(id))
 
       })
